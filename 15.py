26542c5c77d98b9c650c9764bc93e198ac7c62b5
Jinfang Luo <jincute@gmail.com>
2020-02-29 18:07:05 -0500

    Add files via upload

--- 15.py
+++ 15.py
@@ -2,9 +2,7 @@ import pdb
 from typing import List
 class Solution:
     def threeSum(self, nums: List[int]) -> List[List[int]]:
-    '''
-    Time Limit Exceeded
-    '''
+        # passes 311/313, Time limit exceeded
         '''
         R = []
         length_list = len(nums)
@@ -14,7 +12,6 @@ class Solution:
             j = i
             while j < length_list-2:
                 j = j+1
-                #print('j: ', j)
                 sum = nums[i] + nums[j]
                 for k in range(j+1, length_list):
                     if -sum == nums[k]:
@@ -22,7 +19,30 @@ class Solution:
                             R.append(sorted([nums[i], nums[j], nums[k]]))
         return R
         '''
-    
-S = Solution().threeSum([-1, 0, 1, 2, -1, -4])
-#S = Solution().threeSum([1,-1,-1,0])
+        if len(nums) < 3:
+            return []
+        R = []
+        nums.sort()
+        for i in range(len(nums)):
+            if i > 0 and nums[i] == nums[i-1]:
+                continue
+            l, r = i+1, len(nums)-1
+            while l < r:
+                s = nums[i] + nums[l] + nums[r]
+                if s < 0:
+                    l += 1
+                elif s > 0:
+                    r -= 1
+                else:
+                    R.append([nums[i], nums[l], nums[r]])
+                    while l < r and nums[l] == nums[l+1]:
+                        l += 1
+                    while l < r and nums[r] == nums[r-1]:
+                        r -= 1
+                    l += 1
+                    r -= 1
+        return R
+
+#S = Solution().threeSum([2,7,11])
+S = Solution().threeSum([-1,0,1,2,-1,-4])
 print(S)

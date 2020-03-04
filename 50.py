a8e5433034ade0f02ff171d7e841ee39a41dd64c
Jinfang Luo <jincute@gmail.com>
2020-03-04 14:42:41 -0500

    Add files via upload

--- 50.py
+++ 50.py
@@ -3,12 +3,21 @@ class Solution:
         '''
         291 / 304 pass, time limit exceeded
         '''
+        '''
         result = 1
         for i in range(abs(n)):
             result *= x
         if n < 0:
             result = 1/result
         return round(result,5)
+        '''
+        if n == 0:
+            return 1
+        if n <0 :
+            return 1/self.myPow(x,-n)
+        if n%2 ==1:
+            return x*self.myPow(x,n-1)
+        return self.myPow(x*x,n/2)
 
-S = Solution().myPow(0.00001, 2147483647)
+S = Solution().myPow(0.00011, 21254)
 print(S)

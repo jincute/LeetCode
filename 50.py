9895b45345f7f37493aca7720be0f94bcba630a6
Jinfang Luo <jincute@gmail.com>
2020-03-04 14:51:32 -0500

    Add files via upload

--- 50.py
+++ 50.py
@@ -13,7 +13,7 @@ class Solution:
         '''
         if n == 0:
             return 1
-        if n <0 :
+        if n < 0 :
             return 1/self.myPow(x,-n)
         if n%2 ==1:
             return x*self.myPow(x,n-1)

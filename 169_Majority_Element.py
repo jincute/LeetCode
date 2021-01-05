e2b58ba95195acbaff66f4bb0b01319dfaf76c92
Jinfang Luo <jincute@gmail.com>
2021-01-04 21:13:05 -0500

    Add files via upload

--- 169_Majority_Element.py
+++ 169_Majority_Element.py
@@ -4,13 +4,35 @@ class Solution:
         n = len(nums)
         if n == 1:
             return nums[0]
-        mid = n/2
+        mid = n / 2
         # print(mid)
         flag = 1
-        for a in range(0, n-1):
-            if nums[a] == nums[a+1]:
+        for a in range(0, n - 1):
+            if nums[a] == nums[a + 1]:
                 flag += 1
                 if flag - mid > 0:
                     return nums[a]
             else:
                 flag = 1
+
+        '''
+        divide and conquer
+        '''
+        # if len(nums) == 1:
+        #     return nums[0]
+        # mid = len(nums) // 2
+        # L = self.majorityElement(nums[:mid])
+        # R = self.majorityElement(nums[mid:])
+        # if L == R:
+        #     return L
+        # else:
+        #     countl = countr = 0
+        #     for i in range(len(nums)):
+        #         if L == nums[i]:
+        #             countl += 1
+        #         elif R == nums[i]:
+        #             countr += 1
+        #     if countl > countr:
+        #         return L
+        #     else:
+        #         return R

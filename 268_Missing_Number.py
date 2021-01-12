825ca1e7eca74f72813507de500a096a40d8b320
Jinfang Luo <jincute@gmail.com>
2021-01-12 14:10:24 -0500

    Add files via upload

--- 268_Missing_Number.py
+++ 268_Missing_Number.py
@@ -1,5 +1,8 @@
 class Solution:
     def missingNumber(self, nums: List[int]) -> int:
+        '''
+        solution 1
+
         nums.sort()
         init = 0
         for n in nums:
@@ -8,3 +11,17 @@ class Solution:
             else:
                 return init
         return len(nums)
+        '''
+
+        '''
+        solution 2
+        数学方法: 求 0 ~ n+1 的值，减去nums的和
+        '''
+
+        nums_sum = 0
+        total_sum = 0
+        for n in nums:
+            nums_sum += n
+        for i in range(len(nums) + 1):
+            total_sum += i
+        return total_sum - nums_sum
